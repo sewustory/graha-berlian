5471700cb274af854cfcded1067946f3431a156a
sewustory <lynajuniary@gmail.com>
2025-12-22 18:31:53 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,18 @@
-console.log("login.js loaded");
+// js/login.js
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-document.getElementById("btnLogin").onclick = () => {
-  alert("Login diklik");
-  window.location.href = "dashboard.html";
-};
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (err) {
+    alert(err.message);
+  }
+});
