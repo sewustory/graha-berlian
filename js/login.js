a0359456f466a62810c5ca33622a4cee4801903a
sewustory <lynajuniary@gmail.com>
2025-12-22 19:21:06 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,18 +1,20 @@
-// js/login.js
 import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-document.getElementById("loginForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
+const btnLogin = document.getElementById("btnLogin");
+const errorMsg = document.getElementById("errorMsg");
 
+btnLogin.addEventListener("click", async () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+
+  errorMsg.textContent = "";
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
     window.location.href = "dashboard.html";
   } catch (err) {
-    alert(err.message);
+    errorMsg.textContent = "Email atau password salah";
   }
 });
