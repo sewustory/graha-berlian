9b3b5190dc0a304a42745c0e03c8f750493cbf2c
sewustory <lynajuniary@gmail.com>
2025-12-22 18:32:19 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
+// js/auth.js
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
 
